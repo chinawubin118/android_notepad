4210526fd3f86756e5b9b8a1a0a6bffe59ec0891
chinawubin118 <565696580@qq.com>
2017-03-08 23:41:56 +0800

    准备完成详情页面

--- app/src/main/java/com/lotte/android_notepad/ui/activity/NoteDetailActivity.java
+++ app/src/main/java/com/lotte/android_notepad/ui/activity/NoteDetailActivity.java
@@ -1,12 +1,17 @@
 package com.lotte.android_notepad.ui.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.lotte.android_notepad.BaseActivity;
 import com.lotte.android_notepad.R;
 import com.lotte.android_notepad.model.Note;
+
+import java.io.File;
 
 /**
  * 备忘详情页面
@@ -15,6 +20,7 @@ public class NoteDetailActivity extends BaseActivity {
 
     private TextView tvContent;//内容
     private TextView tvDelete, tvReturn;//取消,保存
+    private ImageView ivContent;//图片内容
 
     private Note note;//备忘对象
 
@@ -33,6 +39,7 @@ public class NoteDetailActivity extends BaseActivity {
         tvContent = (TextView) findViewById(R.id.tvContent);
         tvDelete = (TextView) findViewById(R.id.tvDelete);
         tvReturn = (TextView) findViewById(R.id.tvReturn);
+        ivContent = (ImageView) findViewById(R.id.ivContent);
 
         tvDelete.setOnClickListener(this);
         tvReturn.setOnClickListener(this);
@@ -40,6 +47,10 @@ public class NoteDetailActivity extends BaseActivity {
 
     private void setDataToView() {
         tvContent.setText(note.getTitle());
+        if (!TextUtils.isEmpty(note.getImagePath())) {
+            ivContent.setVisibility(View.VISIBLE);
+            Glide.with(this).load(new File(note.getImagePath())).into(ivContent);
+        }
     }
 
     @Override
